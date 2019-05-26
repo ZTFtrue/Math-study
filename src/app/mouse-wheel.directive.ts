57920b1db329b4e3afee792d296edca6ec8ca765
ztftrue <ztftrue@gmail.com>
2019-05-26 14:29:53 +0800

    can conpy

--- src/app/mouse-wheel.directive.ts
+++ src/app/mouse-wheel.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Output, HostListener, EventEmitter, ElementRef } from '@angular/core';
+import { Directive, Output, HostListener, EventEmitter, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appMouseWheel]'
@@ -11,7 +11,9 @@ export class MouseWheelDirective {
   @Output() mouseDown = new EventEmitter();
   @Output() mouseUp = new EventEmitter();
   @Output() mouseLeave = new EventEmitter();
-  constructor() { }
+  @Input() forbidCopy = false;
+  constructor() {
+  }
 
   @HostListener('mousewheel', ['$event']) onMouseWheelChrome(event: any) {
     this.mouseWheelFunc(event);
@@ -28,7 +30,10 @@ export class MouseWheelDirective {
   }
   @HostListener('mousedown', ['$event']) onMouseDown(event: any) {
     this.mouseDown.emit(event);
-    this.elementMouseMove = true;
+    console.log(this.forbidCopy);
+    if ((event.button === 0 && !this.forbidCopy) || event.button === 2 || event.button === 1) {
+      this.elementMouseMove = true;
+    }
     console.log(event);
   }
   @HostListener('mouseleave', ['$event']) onMouseLeave(event: any) {
