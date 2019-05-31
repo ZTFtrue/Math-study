af8e43f9287023316dad3318fca0a1dd9ba5262d
ztftrue <ztftrue@gmail.com>
2019-05-31 18:51:37 +0800

    use mathjx

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { AfterViewInit, Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { MathJax } from 'mathjax';
+// import { MathJax } from 'mathjax';
+declare var MathJax: any;
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
-export class DialogDetailsComponent implements OnInit {
-
+export class DialogDetailsComponent implements AfterViewInit {
+  dataArrary;
   constructor(public dialogRef: MatDialogRef<DialogDetailsComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: string) { }
+    // tslint:disable-next-line: align
+    @Inject(MAT_DIALOG_DATA) public data: string) {
+    this.dataArrary = data.split('\n');
+  }
 
-  ngOnInit() {
 
+
+  ngAfterViewInit() {
+    this.loadMathConfig();
+    this.renderMath();
+  }
+
+  renderMath() {
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub], 'mathContent');
+  }
+  loadMathConfig() {
     MathJax.Hub.Config({
-      tex2jax: { inlineMath: [['$$', '$$'], ['\\(', '\\)']] }
+      showMathMenu: false,
+      tex2jax: { inlineMath: [['$', '$']], displayMath: [['$$', '$$']] },
+      menuSettings: { zoom: 'Double-Click', zscale: '150%' },
+      CommonHTML: { linebreaks: { automatic: true } },
+      'HTML-CSS': { linebreaks: { automatic: true } },
+      SVG: { linebreaks: { automatic: true } }
     });
-
   }
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-
 }
