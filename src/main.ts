e46cd5a11e878c38871d0e417780857a60642d9f
ztftrue <ztftrue@gmail.com>
2019-06-01 18:38:32 +0800

    额

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
-import * as url from 'url';
-let win: BrowserWindow;
-
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-app.on('ready', createWindow);
-
-app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
-});
-
-function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/../../dist/angular-electron/index.html`),
-      protocol: 'file:',
-      slashes: true,
-    })
-  );
-
-  win.webContents.openDevTools();
-
-  win.on('closed', () => {
-    win = null;
-  });
-}
